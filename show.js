15279b66f09cae6d134deef5b9af59ea08e94955
abderahman <abderahman.zahmidi.solicode@gmail.com>
2021-02-10 01:18:48 +0100

    mysit1

---
+++ show.js
@@ -0,0 +1,103 @@
+var a;
+function show1(){
+
+if (a==0){
+    document.getElementById("show").style.display="none";
+    document.getElementById("con").style.display="block";
+    document.getElementById("showSkil").style.display="none";
+    document.getElementById("about").style.display="none";
+    return a=1;
+}
+else{
+    document.getElementById("show").style.display="block";
+    document.getElementById("con").style.display="none";
+    document.getElementById("showSkil").style.display="none";
+    document.getElementById("about").style.display="none";
+    return a=0;
+}
+
+
+}
+
+
+
+var s;
+function show2(){
+    if (s==0){
+        document.getElementById("showSkil").style.display="none";
+        document.getElementById("con").style.display="block";
+        document.getElementById("show").style.display="none";
+        document.getElementById("contMe").style.display="none";
+        document.getElementById("about").style.display="none";
+        return s=1;
+    }
+    else{
+        document.getElementById("showSkil").style.display="block";
+        document.getElementById("con").style.display="none";
+        document.getElementById("show").style.display="none";
+        document.getElementById("contMe").style.display="none";
+        document.getElementById("about").style.display="none";
+
+        return s=0;
+    }
+    
+
+}
+var t;
+function show3(){
+    if (t==0){
+        document.getElementById("showSkil").style.display="none";
+        document.getElementById("con").style.display="block";
+        document.getElementById("show").style.display="none";
+        document.getElementById("contMe").style.display="none";
+        document.getElementById("about").style.display="none";
+        return t=1;
+    }
+    else{
+        document.getElementById("showSkil").style.display="none";
+        document.getElementById("con").style.display="none";
+        document.getElementById("show").style.display="none";
+        document.getElementById("contMe").style.display="block";
+        document.getElementById("about").style.display="none";
+        return t=0;
+    }
+    
+
+}
+function show4(){
+    if (t==0){
+        document.getElementById("showSkil").style.display="none";
+        document.getElementById("con").style.display="block";
+        document.getElementById("show").style.display="none";
+        document.getElementById("contMe").style.display="none";
+        document.getElementById("about").style.display="none";
+        return t=1;
+    }
+    else{
+        document.getElementById("showSkil").style.display="none";
+        document.getElementById("con").style.display="none";
+        document.getElementById("show").style.display="none";
+        document.getElementById("contMe").style.display="none";
+        document.getElementById("about").style.display="block";
+        return t=0;
+    }
+    
+
+}
+
+
+function sendEmail() {
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username : "zahmidi",
+        Password : "andelZ1997",
+        SecureToken: "Generate token here",
+        From : "abderahmanzahmidi@gmail.com",
+        To : 'abderahman.zahmidi.solicode@gmail.com',
+        Subject: "Mail Subject here",
+        Body: "Mail Body Here"
+    })
+        .then(function (message) {
+            alert("mail sent successfully")
+        });
+}
